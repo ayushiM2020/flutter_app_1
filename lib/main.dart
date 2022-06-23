43af7671feb2049a7a78195977fdd2dd2da7ce7f
FlutLab.io <>
2022-06-23 05:12:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,6 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp();
   }
 }
